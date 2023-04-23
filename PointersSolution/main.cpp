8df1eb9a0a797eea14383cdf06c288bab3e65806
Антон <@DESKTOP-C253HS0>
2023-04-23 10:06:59 +0300

    up1

--- PointersSolution/main.cpp
+++ PointersSolution/main.cpp
@@ -1,8 +1,130 @@
 #include <iostream>
 using namespace std;
 
+#define Delimiter "----------------------------------------";
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+int* push_back(int* arr, int& n, int value);
+int* push_front(int* arr, int& n, int value);
+int* insert(int* arr, int& n, int value, int index);
+int* pop_back(int* arr, int& n);
+int* pop_front(int* arr, int& n);
+
 void main() 
 {
 	setlocale(LC_ALL, "");
-	121
+	int n;
+	cout << "¬ведите количество элементов: "; cin >> n;
+	int* arr = new int[n];
+	
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int value;
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+	cout << Delimiter;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	int index;
+	cout << "¬ведите индекс добавл€емого элемента :"; cin >> index;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+	cout << typeid(arr).name() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+
+void Print(int arr[],const int n)
+{
+	cout << typeid(arr).name() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+int* push_back(int* arr, int& n, int value)
+{
+	
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	return arr;
+}
+
+int* push_front(int* arr, int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i+1] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[0] = value;
+	n++;
+	return arr;
+}
+
+int* insert(int* arr, int& n, int value, int index)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	for (int i = index; i < n; i++)buffer[i + 1] = arr[i];
+	//for (int i = 0; i < n; i++)
+	{
+		//if (i < index)buffer[i] = arr[i]; else buffer[i + 1] = arr[i];
+		 //(i < index ? buffer[i] : buffer[i + 1]) = arr[i];
+		
+	}
+
+	buffer[index] = value;
+	delete[] arr;
+	arr = buffer;
+	n++;
+	return arr;
+}
+
+int* pop_back(int* arr, int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	
+	delete[] arr;
+	return buffer;
+}
+
+int* pop_front(int* arr, int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i+1];
+
+	delete[] arr;
+	return buffer;
 }
